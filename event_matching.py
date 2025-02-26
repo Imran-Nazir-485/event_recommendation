14d623793cb01bd24e1bfe1f5873630819ac197b
Imran_Nazir <72697701+Imran-Nazir-485@users.noreply.github.com>
2025-02-26 15:08:06 +0500

    Update event_matching.py

--- event_matching.py
+++ event_matching.py
@@ -11,19 +11,22 @@ from sentence_transformers import SentenceTransformer
 # Load Sentence Transformer model
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
-df=pd.read_excel("events_summary_embeddings.xlsx")
+df=pd.read_excel("events_summary.xlsx")
 
+
+df['embedding'][:100] = df['event_summary'][:100].apply(lambda x: model.encode(x))
 
 def get_embedding(text_emb):
   t=[ast.literal_eval(i) for i in text_emb.split()[1:-1]]
   t=torch.tensor(t)
   return t
 
-# Apply sentence embedding transformation
-df['embedding_tensor']=df['embedding'].apply(get_embedding)
+# # Apply sentence embedding transformation
+# df['embedding_tensor']=df['embedding'].apply(get_embedding)
 
-t=df['embedding_tensor'][0]
+# t=df['embedding_tensor'][0]
 
+t=df['embedding'][0].reshape(1, -1)
 
 st.title("Event Matching")
 
@@ -31,7 +34,7 @@ input=st.text_input("Enter")
 if st.button("Recommend"):
 
   # Compute cosine similarity
-  df['similarity'] = df['embedding_tensor'].apply(lambda x: cosine_similarity(x.reshape(1, -1),t))
+  df['similarity'] = df['embedding'].apply(lambda x: cosine_similarity(x,t[0][0]))
   
   # Filter rows with similarity >= 50% (0.5)
   similar_texts = df[df['similarity'] >= 0.8].sort_values(by='similarity', ascending=False)
