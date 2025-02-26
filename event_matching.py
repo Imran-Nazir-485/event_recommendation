696d24eda7afce0d8c154761858e6498129b2af2
Imran_Nazir <72697701+Imran-Nazir-485@users.noreply.github.com>
2025-02-26 14:46:05 +0500

    Update event_matching.py

--- event_matching.py
+++ event_matching.py
@@ -3,17 +3,22 @@ import streamlit as st
 from langchain_openai import ChatOpenAI
 import os
 import torch
+import ast
 from langchain.embeddings import HuggingFaceEmbeddings
 from sentence_transformers import SentenceTransformer
 # Load Sentence Transformer model
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
+df=pd.read_excel("events_summary_embeddings.xlsx")
 
-df=pd.read_excel("events_summary.xlsx")
 
+def get_embedding(text_emb):
+  t=[ast.literal_eval(i) for i in text_emb.split()[1:-1]]
+  t=torch.tensor(t)
+  return t
 
 # Apply sentence embedding transformation
-df['embedding'] = df['event_summary'].apply(lambda x: model.encode(x))
+df['embedding_tensor']=df['embedding'].apply(get_embedding)
 
 st.title("Event Matching")
 
