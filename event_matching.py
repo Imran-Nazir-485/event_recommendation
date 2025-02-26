24644a9afb15ebc0d49630c0524b1658f8d47fb0
Imran_Nazir <72697701+Imran-Nazir-485@users.noreply.github.com>
2025-02-26 15:34:06 +0500

    Update event_matching.py

--- event_matching.py
+++ event_matching.py
@@ -34,7 +34,7 @@ input=st.text_input("Enter")
 if st.button("Recommend"):
   
   # Compute cosine similarity
-  df['similarity'] = df['embedding'][:100].apply(lambda x: cosine_similarity(x.reshape(1,-1),t.reshape(1,-1)))
+  df['similarity'] = df['embedding'][:100].apply(lambda x: cosine_similarity(x.reshape(1,-1),t.reshape(1,-1))[0][0])
   
   # Filter rows with similarity >= 50% (0.5)
   st.dataframe(df)
