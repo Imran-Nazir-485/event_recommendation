21c23ccf9b2a77390b60d98b0c24f8ab59a54850
Imran_Nazir <72697701+Imran-Nazir-485@users.noreply.github.com>
2025-03-02 19:40:38 +0500

    Update event_matching.py

--- event_matching.py
+++ event_matching.py
@@ -151,7 +151,7 @@ if selection=="Event Recommendation":
   similar_texts=df[df['similarity'] >= 0.50].sort_values(by='similarity', ascending=False)
 
 #   # # Display results
-  st.write(similar_texts[['similarity','title', 'location', 'address', 'category','description', 'organizer','tags']])
+  st.dataframe(similar_texts[['similarity','title', 'location', 'address', 'category','description', 'organizer','tags']])
   # st.write(df)
 
 
