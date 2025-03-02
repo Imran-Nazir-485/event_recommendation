0a7e8b4481638f01c6eab4de24fa9cc4dc6a66ea
Imran_Nazir <72697701+Imran-Nazir-485@users.noreply.github.com>
2025-03-02 19:34:00 +0500

    Update event_matching.py

--- event_matching.py
+++ event_matching.py
@@ -136,14 +136,14 @@ if selection=="Event Recommendation":
 
   # Ensure both are numpy arrays
   embedding_1 = np.array(df['embedding'][0]).reshape(1, -1)
-  embedding_2 = np.array(embeddings).reshape(1, -1)
+  embedding_2 = np.array(embeddings[0).reshape(1, -1)
 
   st.write(type(embedding_1))
   st.write(type(embedding_2))
 
   
   # Compute cosine similarity
-  # similarity = cosine_similarity(embedding_1, embedding_2)
+  similarity = cosine_similarity(embedding_1, embedding_2)
   
   # cosine_similarity([df['embedding'][0]],embeddings.reshape(1,-1))
   # .apply(lambda x: cosine_similarity([x],embeddings.reshape(1,-1)))
