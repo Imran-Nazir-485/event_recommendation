9b5a9f47a4c56334a003c523b76881f31a12be6a
Imran_Nazir <72697701+Imran-Nazir-485@users.noreply.github.com>
2025-03-03 12:29:38 +0500

    Update event_matching.py

--- event_matching.py
+++ event_matching.py
@@ -320,7 +320,7 @@ if selection=="My Profile":
   
   # Display interests in a flex container
   st.markdown('<div class="interest-container">', unsafe_allow_html=True)
-  for interest in user_data["interests"][:3]:
+  for interest in user_data["interests"][:2]:
       st.markdown(f'<span class="interest">{interest}</span>', unsafe_allow_html=True)
   st.markdown('</div>', unsafe_allow_html=True)
   
